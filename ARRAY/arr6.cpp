e181e7d4bc0d02b387ccaede5fe830c31667c0b7
surajkumardev <blackmesd@gmail.com>
2025-07-30 21:58:57 +0530

    adding values

--- ARRAY/arr6.cpp
+++ ARRAY/arr6.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-//Jist -> WAP to swap min and max number of the array 
+//Jist -> WAP to swap min and max number of the array Not done 
 int main(){
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int n = 10;
@@ -17,8 +17,6 @@ int main(){
             maxIndex = i;
         }
     }
-
-    
 
     // Swap min and max elements
     swap(arr[minIndex], arr[maxIndex]);
