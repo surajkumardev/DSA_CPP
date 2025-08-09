485f9880d2a61183dec992fc79193d79c2351d87
surajkumardev <blackmesd@gmail.com>
2025-08-09 11:52:41 +0530

    making changes

---
+++ Pointer/poi_0.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//Jist => 
+// 1. what is pointer 
+// 2. what is two pointer_safety
+// 3. what is ampersand
+// 4. pass by value 
+// 5. pass by referece 
+// 6. array pionter 
+int main(){
+    
+
+    return 0;
+}
