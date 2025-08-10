f4a8736668ec8613b97a43a60c0cadbdfdb0559c
surajkumardev <blackmesd@gmail.com>
2025-08-10 09:45:03 +0530

    adding changes

---
+++ String/str_2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<vector>
+using namespace std;
+
+//check wherther the given string is palindrome or not
+//tolower-> tolower(ch) to conver to lower case 
+
+int main(){
+    
+
+return 0;
+}
