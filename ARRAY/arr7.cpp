f5829694fc53c601717443ad2cc5c0e6c89e8bf9
surajkumardev <blackmesd@gmail.com>
2025-08-25 19:28:39 +0530

    adding changes

--- ARRAY/arr7.cpp
+++ ARRAY/arr7.cpp
@@ -5,7 +5,7 @@ int main() {
     int arr[] = {1, 2,3,4,5};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) { //sdfdsf
         int count = 0;
 
         for(int j = 0; j < n; j++) {
