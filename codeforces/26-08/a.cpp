79411418d65a99faa983f10ef85fa131e109324d
surajkumardev <blackmesd@gmail.com>
2025-08-26 20:48:18 +0530

    adding chnage

---
+++ codeforces/26-08/a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+
+
+using namespace std;
+
+
+
+int main(){
+    int t; 
+    cin>>t; 
+    while(t--> 0){
+        int n;  //2
+        cin>> n; 
+        
+    }
+    
+    return 0;
+
+
+}
